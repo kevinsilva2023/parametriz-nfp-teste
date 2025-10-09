69f3ef378a55a0d8c8a4dbc163be8ab5dc8faf51
Mauricio Kamikoga <mau.mitsuo@gmail.com>
2025-10-08 22:17:18 -0300

    Inclui Nome e Instituicao no UserToken na API

---
+++ Server/src/Parametriz.AutoNFP.Api/Migrations/20251009004309_RefreshTokenUsuarioId.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Parametriz.AutoNFP.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefreshTokenUsuarioId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "RefreshTokens");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UsuarioId",
+                table: "RefreshTokens",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UsuarioId",
+                table: "RefreshTokens");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "RefreshTokens",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
