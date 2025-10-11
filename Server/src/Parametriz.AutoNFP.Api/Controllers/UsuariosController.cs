bca0290fac0abc897f2e3f6cb40dda6ce252fee6
Mauricio Kamikoga <mau.mitsuo@gmail.com>
2025-10-10 22:56:03 -0300

    Acrescenta Email, Administrador e Desativado em ObterPorFiltros de Usuarios na API

--- Server/src/Parametriz.AutoNFP.Api/Controllers/UsuariosController.cs
+++ Server/src/Parametriz.AutoNFP.Api/Controllers/UsuariosController.cs
@@ -6,6 +6,7 @@ using Parametriz.AutoNFP.Api.Application.Usuarios.Services;
 using Parametriz.AutoNFP.Api.Extensions;
 using Parametriz.AutoNFP.Api.Models.User;
 using Parametriz.AutoNFP.Api.ViewModels.Usuarios;
+using Parametriz.AutoNFP.Domain.Core.Enums;
 using Parametriz.AutoNFP.Domain.Core.Notificacoes;
 using Parametriz.AutoNFP.Domain.Usuarios;
 
@@ -42,9 +43,12 @@ namespace Parametriz.AutoNFP.Api.Controllers
         }
 
         [HttpGet("obter-por-filtros")]
-        public async Task<IEnumerable<UsuarioViewModel>> Get(string nome = "")
+        public async Task<IEnumerable<UsuarioViewModel>> Get(string nome = "", string email = "", 
+            BoolTresEstados administrador = BoolTresEstados.Ambos, BoolTresEstados desativado = BoolTresEstados.Falso)
         {
-            return (await _usuarioRepository.ObterPorFiltros(InstituicaoId, nome)).ToViewModel();
+            return (await _usuarioRepository
+                .ObterPorFiltros(InstituicaoId, nome, email, administrador, desativado))
+                .ToViewModel();
         }
 
         [HttpPost]
