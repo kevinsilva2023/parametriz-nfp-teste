b7b88ac787e6cd90f22d5f623c28e5037a72ed26
Kevin Silva <kevinmarcos280@gmail.com>
2025-10-12 13:23:57 -0300

    Tira chamada de components filhos de configuracoes por rotas

--- Client/Parametriz.AutoNFP/src/app/configuracoes/configuracoes.module.ts
+++ Client/Parametriz.AutoNFP/src/app/configuracoes/configuracoes.module.ts
@@ -12,14 +12,60 @@ import { errorInterceptor } from 'src/app/shared/interceptors/error.interceptor'
 import { jwtInterceptor } from 'src/app/shared/interceptors/jwt.interceptor';
 import { UsuarioService } from './usuario/services/usuario.service';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { VoluntarioComponent } from './voluntario/voluntario.component';
+import { CadastrarVoluntarioComponent } from './voluntario/cadastrar-voluntario/cadastrar-voluntario.component';
+import { VisualizarVoluntarioComponent } from './voluntario/visualizar-voluntario/visualizar-voluntario.component';
+
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSelectModule } from '@angular/material/select';
+import { MatMenuModule } from '@angular/material/menu';
+
+
+
+import { AlertModule } from 'ngx-bootstrap/alert';
+
+
+import { UsuarioComponent } from './usuario/usuario.component';
+import { CadastrarUsuarioComponent } from './usuario/components/cadastrar-usuario/cadastrar-usuario.component';
+import { ListarUsuarioComponent } from './usuario/components/listar-usuario/listar-usuario.component';
+import { DesativarUsuarioComponent } from './usuario/components/desativar-usuario/desativar-usuario.component';
+import { AtivarUsuarioComponent } from './usuario/components/ativar-usuario/ativar-usuario.component';
+import { EditarUsuarioComponent } from './usuario/components/editar-usuario/editar-usuario.component';
+import { PefilUsuarioComponent } from './usuario/components/pefil-usuario/pefil-usuario.component';
+
+
+
 @NgModule({
   declarations: [
     ConfiguracoesComponent,
+    VoluntarioComponent,
+    CadastrarVoluntarioComponent,
+    VisualizarVoluntarioComponent,
+    UsuarioComponent,
+    CadastrarUsuarioComponent,
+    ListarUsuarioComponent,
+    DesativarUsuarioComponent,
+    AtivarUsuarioComponent,
+    EditarUsuarioComponent,
+    PefilUsuarioComponent,
   ],
   imports: [
     CommonModule,
     ConfiguracoesRoutingModule,
     NgbModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSlideToggleModule,
+    MatSelectModule,
+    AlertModule,
+    MatInputModule,
+    MatMenuModule
   ],
   providers: [
     UsuarioService,
--- Client/Parametriz.AutoNFP/src/app/configuracoes/usuario/usuario.component.ts
+++ Client/Parametriz.AutoNFP/src/app/configuracoes/usuario/usuario.component.ts
@@ -1,32 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { combineLatest, map, startWith, Observable } from 'rxjs';
-import { DecimalPipe } from '@angular/common';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { CadastrarUsuarioComponent } from './components/cadastrar-usuario/cadastrar-usuario.component';
 import { LocalStorageUtils } from 'src/app/shared/utils/local-storage-utils';
-
 
 @Component({
   selector: 'app-usuario',
   standalone: false,
   templateUrl: './usuario.component.html',
 })
-export class UsuarioComponent implements OnInit {
+export class UsuarioComponent  {
 
-  isAdministrador = false;
+  isAdministrador = true;
 
-  usuarioAtivoEhAdmin() {
-    const usuario = LocalStorageUtils.obterUsuario();
 
-    this.isAdministrador = (usuario?.claims || [])
-      .some((claim: { type: string; value: string; }) => claim?.type === 'role' && claim?.value === 'Administrador');
-
-    console.log('É administrador?', this.isAdministrador);
-
-  }
-
-  ngOnInit(): void {
-    this.usuarioAtivoEhAdmin();
-  }
 }
