26ed9b42da4b0e9cf6a9e8e3718b93c772e57ce1
Mauricio Kamikoga <mau.mitsuo@gmail.com>
2025-10-15 23:28:23 -0300

    Inclui base inicial do projeto Background

---
+++ Server/src/BackgroundServices/Parametriz.AutoNFP.ConsoleApp/Configs/BuilderConfig.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parametriz.AutoNFP.ConsoleApp.Configs
+{
+    public static class BuilderConfig
+    {
+        public static IHostBuilder AddAppConfiguration(this IHostBuilder builder)
+        {
+            builder.ConfigureAppConfiguration((context, builder) =>
+            {
+                builder
+                    .SetBasePath(context.HostingEnvironment.ContentRootPath)
+                    .AddJsonFile("appsettings.json", true, true)
+                    .AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", true, true)
+                    .AddEnvironmentVariables();
+            });
+
+            return builder;
+        }
+    }
+}
---
+++ Server/src/BackgroundServices/Parametriz.AutoNFP.ConsoleApp/SeleniumConfig/WebDriverFactory.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parametriz.AutoNFP.ConsoleApp.SeleniumConfig
+{
+    public static class WebDriverFactory
+    {
+        public static IWebDriver CreateWebDriver(int port, bool headless)
+        {
+            IWebDriver webDriver = null;
+
+            var options = new ChromeOptions();
+
+            if (headless)
+                options.AddArgument("--headless");
+
+            var uri = new Uri($"http://localhost:{port}/");
+
+            webDriver = new RemoteWebDriver(uri, options);
+
+            return webDriver;
+        }
+    }
+}
