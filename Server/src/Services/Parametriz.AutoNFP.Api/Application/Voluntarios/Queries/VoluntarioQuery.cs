3650e0226939aa17c9b71dd6efac69c2e92203c7
Mauricio Kamikoga <mau.mitsuo@gmail.com>
2025-10-22 23:41:07 -0300

    Inclui EmailConfirmado em Voluntarios na API

--- Server/src/Services/Parametriz.AutoNFP.Api/Application/Voluntarios/Queries/VoluntarioQuery.cs
+++ Server/src/Services/Parametriz.AutoNFP.Api/Application/Voluntarios/Queries/VoluntarioQuery.cs
@@ -1,4 +1,5 @@
-﻿using Parametriz.AutoNFP.Api.Extensions;
+﻿using Microsoft.AspNetCore.Identity;
+using Parametriz.AutoNFP.Api.Extensions;
 using Parametriz.AutoNFP.Api.ViewModels.Voluntarios;
 using Parametriz.AutoNFP.Core.Enums;
 using Parametriz.AutoNFP.Domain.Certificados;
@@ -9,10 +10,13 @@ namespace Parametriz.AutoNFP.Api.Application.Voluntarios.Queries
     public class VoluntarioQuery : IVoluntarioQuery
     {
         private readonly IVoluntarioRepository _voluntarioRepository;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public VoluntarioQuery(IVoluntarioRepository voluntarioRepository)
+        public VoluntarioQuery(IVoluntarioRepository voluntarioRepository, 
+                               UserManager<IdentityUser> userManager)
         {
             _voluntarioRepository = voluntarioRepository;
+            _userManager = userManager;
         }
 
         public async Task<IEnumerable<VoluntarioViewModel>> ObterPorFiltros(Guid instituicaoId, string nome = "", string email = "", 
@@ -21,10 +25,18 @@ namespace Parametriz.AutoNFP.Api.Application.Voluntarios.Queries
         {
             var voluntarios = await _voluntarioRepository.ObterPorFiltros(instituicaoId, nome, email, administrador, desativado);
 
-            return voluntarios
+            var voluntariosViewModel = voluntarios
                 .Where(v => certificadoStatus == null || v.Certificado?.Status == certificadoStatus)
                 .ToList()
                 .ToViewModel();
+
+            foreach (var voluntarioViewModel in voluntariosViewModel)
+            {
+                var user = await _userManager.FindByIdAsync(voluntarioViewModel.Id.ToString());
+                voluntarioViewModel.EmailConfirmado = user.EmailConfirmed;
+            }
+
+            return voluntariosViewModel;
         }
     }
 }
